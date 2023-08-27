14a43c4482a65d1d53044fd0f282b2f62f9b3392
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2023-08-28 00:05:03 +0200

    refactor: improve about section paragraph text balancing

--- src/app/(home)/about.tsx
+++ src/app/(home)/about.tsx
@@ -4,8 +4,8 @@ import { Container } from "@/components/container";
 export const About = () => (
   <section className="relative bg-gradient-section py-20" id="about">
     <Container>
-      <p className="max-w-prose text-center text-2xl lg:text-3xl xl:text-4xl">
-        <Balancer>
+      <p className="text-center text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
+        <Balancer preferNative={false}>
           As a web developer passionate about frontend, I strive to solve
           problems in simple, performant, cost-effective and scalable ways while
           delivering the best user experience. I am a strong advocate for modern
